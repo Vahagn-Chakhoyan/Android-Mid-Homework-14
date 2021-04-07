5be95dc54e57760580b97fd42c4aa69925ad79f2
Vahagn Chakhoyan <vahagn.chakhoyan@mail.ru>
2021-04-07 11:13:59 +0400

    initial comit

---
+++ app/src/main/java/com/vahagn/androidmidhomework14/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vahagn.androidmidhomework14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.coroutineContext
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
